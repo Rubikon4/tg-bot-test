4ee8e4e8f966c94230715d35ad4b690c3198684f
Rubikon4 <vazintag@yandex.ru>
2025-10-01 18:03:31 +0300

    v0.2. - Add some functionale, add Handlers file, run.py now is fully Entry point.

---
+++ handlers.py
@@ -0,0 +1,28 @@
+from aiogram import Router, F
+from aiogram.filters import CommandStart, Command
+from aiogram.types import Message
+
+router = Router()
+
+@router.message(CommandStart())
+async def   cmd_start(message: Message):
+    await message.answer('Hello! bot is running... Type /help for commands list.')
+@router.message(Command('help'))
+async def get_help(message: Message):
+    await message.answer('/help to get to get commands list\n/meme to get funny meme lol\n/dice to throw dice\n/getID to get your personal ID')
+@router.message(Command('dice'))
+async def get_help(message: Message):
+    await message.answer_dice()
+@router.message(Command('getID'))
+async def get_help(message: Message):
+    await message.reply(f'Your ID: {message.from_user.id}\nYour name: {message.from_user.first_name}') # reply для ОТВЕТА на сообщение (как функция тг)
+@router.message(Command('meme'))
+async def get_help(message: Message):
+    await message.answer_photo(photo='AgACAgIAAxkBAAMxaN0uaIVb7R4yLWBLZORJ0U3IUVEAAtD7MRueBelKtCJgPakJ0xIBAAMCAAN5AAM2BA',
+                               caption='AHAHAHAHAHAHAHH AHAHHAH AHHA haha.....')
+@router.message(F.text=='Hello')
+async def say_hello(message: Message):
+    await message.answer('Hello mate!')
+@router.message(F.photo)                            
+async def get_photo(message: Message):
+    await message.answer(f'Your Photo_ID: {message.photo[-1].file_id}')
--- run.py
+++ run.py
@@ -1,22 +1,14 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart, Command
-from aiogram.types import Message
-
 from config import TOKEN
+from handlers import router
 
 bot = Bot(token=TOKEN) # токен бота импортируйте из config.py, который в свою очередь создайте в той же папке, что и run.py
 dp = Dispatcher()
 
-@dp.message(CommandStart())
-async def   cmd_start(message: Message):
-    await message.answer('Hello! bot is running...')
-@dp.message(Command('help'))
-async def get_help(message: Message):
-    await message.answer('This is /help function.')
-
 async def main():
+    dp.include_router(router)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
